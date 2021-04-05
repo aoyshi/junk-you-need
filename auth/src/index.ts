e78f43d81d2466cf8a65740133f4909b22cfed56
aoyshi <37639117+aoyshi@users.noreply.github.com>
2021-04-04 20:12:05 -0400

    added routers for all auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 
+import { currentUserRouter } from './routes/current-user';
+import { signUpRouter } from './routes/signup';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+
 const PORT = 3000;
 const app = express();
 app.use(express.json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.status(200).send('I WORK!');
-});
+app.use(currentUserRouter);
+app.use(signUpRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
