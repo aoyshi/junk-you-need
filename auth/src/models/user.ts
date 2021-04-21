27c3789897e708b867fdccb32791a3d9c0092dbe
aoyshi <37639117+aoyshi@users.noreply.github.com>
2021-04-20 22:31:44 -0400

    added user model and sign up feature

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,11 +1,24 @@
 import mongoose from 'mongoose';
 
+// describes attributes to create new User (only props added by dev, not by mongoose)
 interface UserAttrs {
   email: string;
   password: string;
 }
 
-const UserSchema = new mongoose.Schema({
+// describes User Model: added custom method "build"
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// describes User Document
+// if mongoose added extra props BTS (createdAt, updatedAt), list them here
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -16,6 +29,12 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', UserSchema);
+// New "build" function wraps new User creation
+// ensures TS can typecheck attrs before creation
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export { User };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,24 +2,36 @@ import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 
 import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
 router.post(
-  '/api/users/sigup',
+  '/api/users/signup',
   [
     body('email').isEmail().withMessage('You must enter a valid email address'),
     body('password')
       .isLength({ min: 4, max: 12 })
       .withMessage('Password must be between 4 and 12 characters long!'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
 
-    res.status(200).send('YOU SHALL NOW PASS!');
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError('Email already in use.');
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+    res.status(200).send(user);
   }
 );
 
